8d721a0009b38af08a00e54e85e96726407e5ccb
Silverhawk <sondra.silverhawk@hush.com>
2018-10-04 20:17:56 -0700

    Add basic files for p5 project

---
+++ README.md
@@ -0,0 +1,1 @@
+# bread-identifier.github.io
---
+++ sketch.js
@@ -0,0 +1,9 @@
+// Executes at the beginning
+function setup() {
+  // put setup code here
+}
+
+// Looping at framerate 
+function draw() {
+  // put drawing code here
+}
